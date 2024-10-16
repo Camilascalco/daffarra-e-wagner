49609f306d88532ce0a3a21b345566f486f4f197
Maria Texdorf <125214160+mariatexdorf07@users.noreply.github.com>
2024-10-16 08:17:30 -0300

    Add files via upload

---
+++ maria1.sqlite.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE tutor(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  endereco text NOT NULL,
+  nome text NOT NULL,
+  telefone text NOT NULL,
+  cpf text NOT NULL,
+  email text NOT NULL
+ );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
